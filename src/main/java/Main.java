89f8462a5b3a288a08da500d323fc3620e4f8e4b
Harshith <chandanapu.25bcs10263@sst.scaler.com>
2026-06-18 22:34:36 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
          
          Scanner scanner = new Scanner(System.in);
          
@@ -29,5 +29,26 @@ public class Main {
             }
             
          }
+         scanner.close();
+    }
+    public static String type(String command){
+        String commands[] = {"exit","type","echo"};
+        String path = System.getenv("PATH");
+        String pathDirs[] = path.split(";");
+
+        for(int i = 0; i<commands.length; i++){
+            if(commands[i].equals(command)){
+                return command+" is a shell builtin" ;
+            }
+        }
+
+        for(int i = 0; i<pathDirs.length; i++){
+            File file  = new File(pathDirs[i],command);
+            if(file.exists() && file.canExecute()){
+                return command+" is "+ file.getAbsolutePath();
+            }
+        }
+        return command+": not found";
+        
     }
 }
